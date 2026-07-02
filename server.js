1919c7f08bb2b7c7868b611b402883c57dffbb6f
Hashir Yamin <hashiryamin@gmail.com>
2026-07-02 17:37:48 +0500

    Improve admin panel UI

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "work_order_session_secret",
